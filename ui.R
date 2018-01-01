925b70ec5c0aa578767648c7c7ab51857e5a9bfd
Mike Russell <mike.russell@gov.ab.ca>
2018-01-01 15:31:57 -0700

    Update 1.1.2018
    Changed some of the names of outputs for clarity

--- ui.R
+++ ui.R
@@ -16,25 +16,24 @@ shinyUI(pageWithSidebar(
     fileInput('MegaDB', 'Step 1. Choose your Access database to commence distance sampling analysis',
               accept=c('.accdb', '.mdb')),
     fileInput('WMU_Shp', 'Step 2. Choose your WMU Polygon Shapefile File (Alberta 10TM) - Note include all shapefile components (i.e. *.shp, *.dbf, *.sbn, etc.)',
-             accept=c('.shp','.dbf','.sbn','.sbx', '.shx','.prj','.cpg'), multiple=TRUE),
-   sliderInput("truncation", "Step 3: Choose right truncation distance", min=0, max=1000, value=800, step = 25)
-    
-               ),
+              accept=c('.shp','.dbf','.sbn','.sbx', '.shx','.prj','.cpg'), multiple=TRUE),
+    sliderInput("truncation", "Step 3: Choose right truncation distance", min=0, max=1000, value=425, step = 25)
+
+  ),
   mainPanel(
-     tabsetPanel(
-       tabPanel("Moose",textOutput("MOOS_TXT"), DT::dataTableOutput('MOOS_TAB'), plotOutput("myplot2"),
-                     plotOutput("MOOS_QQ"), plotOutput("myplot"), uiOutput("plots")),
-            tabPanel("Mule Deer", textOutput("MD_TXT"), plotOutput("MUDE_MAP"),
-                     plotOutput("myplot3"),
-                     plotOutput("MUDE_QQ")),
-            tabPanel("White-tailed Deer", plotOutput("WTDE_MAP"),
-                     plotOutput("WTDE_DF"),
-                     plotOutput("WTDE_QQ")),
-            tabPanel("Elk", plotOutput("WAPT_MAP"),
-                     plotOutput("WAPT_DF"),
-                     plotOutput("WAPT_QQ")),
-            tabPanel("Power analysis")
+    tabsetPanel(
+      tabPanel("Moose",textOutput("MOOS_TXT"), DT::dataTableOutput('MOOS_TAB'), plotOutput("MOOS_DF"),
+               plotOutput("MOOS_QQ"), plotOutput("MOOS_MAP"), uiOutput("plots")),
+      tabPanel("Mule Deer", plotOutput("MUDE_MAP"),#textOutput("MD_TXT"), plotOutput("MUDE_MAP"),
+               plotOutput("MD_DF"),
+               plotOutput("MUDE_QQ")),
+      tabPanel("White-tailed Deer", plotOutput("WTDE_MAP"),
+               plotOutput("WTDE_DF"),
+               plotOutput("WTDE_QQ")),
+      tabPanel("Elk", plotOutput("WAPT_MAP"),
+               plotOutput("WAPT_DF"),
+               plotOutput("WAPT_QQ")),
+      tabPanel("Power analysis")
 
-  ))
+    ))
 ))
-
